04823f7e77c81a2191432f5954130eb524478de6
Andrii Posternak <posternak.andrii1@gmail.com>
2026-05-21 14:01:39 +0300

    Refactorings, make the code a better place.

--- src/ComposerJsonFile.php
+++ src/ComposerJsonFile.php
@@ -2,20 +2,18 @@
 
 namespace Posternak\ComposerFile;
 
-use Composer\Json\JsonManipulator;
 use Posternak\JsonFile\JsonFile;
 use RuntimeException;
 
 class ComposerJsonFile extends JsonFile {
-    public function __construct(private readonly string $filePath) {
-        parent::__construct($filePath);
-    }
-
     public function getPackageVersionConstraint(string $packageName): ?string {
-        $packages = array_merge($this->section('require'), $this->section('require-dev'));
-        $value = $packages[$packageName] ?? null;
+        $packages = array_merge(
+            $this->section('require'),
+            $this->section('require-dev')
+        );
+        $versionConstraint = $packages[$packageName] ?? null;
 
-        return is_string($value) ? $value : null;
+        return is_string($versionConstraint) ? $versionConstraint : null;
     }
 
     public function setPackageVersionConstraint(string $packageName, string $versionConstraint): self {
@@ -25,20 +23,15 @@ class ComposerJsonFile extends JsonFile {
             default => throw new RuntimeException("Package '$packageName' not found in 'require' or 'require-dev'"),
         };
 
-        if (false === $contents = file_get_contents($this->filePath)) {
-            throw new RuntimeException("Cannot read '{$this->filePath}'");
-        }
-
-        $manipulator = new JsonManipulator($contents);
-        $manipulator->addLink($section, $packageName, $versionConstraint);
-        file_put_contents($this->filePath, $manipulator->getContents());
+        $this->set("$section.$packageName", $versionConstraint);
+        $this->save();
 
         return $this;
     }
 
     /** @return array<array-key, mixed> */
-    private function section(string $name): array {
-        $value = $this->has($name) ? $this->get($name) : [];
+    private function section(string $path): array {
+        $value = $this->has($path) ? $this->get($path) : [];
         return is_array($value) ? $value : [];
     }
 }
